1d0d070111effd6acd68b25af7effd23e983eecd
songjeonghyun11 <thdwjd7899@naver.com>
2024-04-05 16:50:07 +0900

    쿠폰 관련 서비스 로직 작성 및 QueryDSL 추가 - 쿠폰 관련 서비스 작성 - 테스트 코드 완료

--- coupon-core/src/main/java/com/example/couponcore/model/Coupon.java
+++ coupon-core/src/main/java/com/example/couponcore/model/Coupon.java
@@ -1,12 +1,17 @@
 package com.example.couponcore.model;
 
+import com.example.couponcore.exception.CouponIssueException;
+import com.example.couponcore.exception.ErrorCode;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.security.PublicKey;
 import java.time.LocalDateTime;
+
+import static com.example.couponcore.exception.ErrorCode.*;
 
 @Builder
 @NoArgsConstructor
@@ -44,5 +49,29 @@ public class Coupon extends BaseTimeEntity{
 
     @Column(nullable = false)
     private LocalDateTime dateIssueEnd;
+
+    public boolean availableIssueQuantity() {
+        // 쿠폰타입에서 쿠폰의 발행개수가 선착순이 아니며 무제한이면 null로 표현
+        if (totalQuantity == null) {
+            return true;
+        }
+        return totalQuantity > issuedQuantity;
+    }
+
+    public boolean availableIssueDate() {
+        LocalDateTime now = LocalDateTime.now();
+        return dateIssueStart.isBefore(now) && dateIssueEnd.isAfter(now);
+    }
+
+    public void issue() {
+        if(!availableIssueQuantity()) {
+            throw new CouponIssueException(INVALID_COUPON_ISSUE_QUANTITY, "발급 가능한 수량을 초과합니다. total : %s, issued: %s".formatted(totalQuantity, issuedQuantity));
+        }
+
+        if(!availableIssueDate()) {
+            throw new CouponIssueException(INVALID_COUPON_ISSUE_DATE, "발급 가능한 일자가 아닙니다. request : %s, issueStart: %s, issueEnd: %s".formatted(LocalDateTime.now(), dateIssueStart, dateIssueEnd));
+        }
+        issuedQuantity++;
+    }
 
 }
